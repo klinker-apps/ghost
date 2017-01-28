cdd5506798e099ddb1a9e77ff28becaf314ee937
Jake Klinker <jklinker1@gmail.com>
2017-01-27 18:40:33 -0800

    add ssl to prod config

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://blog.klinkerapps.com/',
+        url: 'https://blog.klinkerapps.com/',
         fileStorage: false,
         forceAdminSSL: true,
         mail: {},
